a9571ad1c92fb5a8df40a5c6c8ae51b1049f7e90
hxw319726 <1140480831@qq.com>
2017-11-02 10:27:42 +0800

    看md

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 Android权威编程指南第24章电视遥控器应用
 
 1. 锁定activity的视图以竖直方向展示`android:screenOrientation="portrait"`
+2. 确保表格布局的列都有同样的宽度`android:stretchColumns="*"`
+3. 提取公共样式到styles中
+     `<style name="RemoteButton">
+         <item name="android:layout_width">0dp</item>
+         <item name="android:layout_height">match_parent</item>
+     </style>`
+     `style="@style/RemoteButton"`
+4. include引入布局文件.xml
+        `<include
+                 layout="@layout/button_row"
+                 android:layout_weight="1"/>`
+
+
--- app/src/main/java/com/haiwell/android/remotecontrol/RemoteControlFragment.java
+++ app/src/main/java/com/haiwell/android/remotecontrol/RemoteControlFragment.java
@@ -1,10 +1,100 @@
 package com.haiwell.android.remotecontrol;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 /**
  * Created by RJ-HXW on 2017/11/1.
  */
 
 public class RemoteControlFragment extends Fragment {
+
+    private TextView mSelectedTextView;
+    private TextView mWorkingTextView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_remote_control, container, false);
+
+        mSelectedTextView = ((TextView) view.findViewById(R.id.fragment_remote_control_selectedTextView));
+        mWorkingTextView = ((TextView) view.findViewById(R.id.fragment_remote_control_workingTextView));
+
+        View.OnClickListener numberButtonListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = ((TextView) view);
+                String working = mWorkingTextView.getText().toString();
+                String text = textView.getText().toString();
+                if (working.equals("0")) {
+                    mWorkingTextView.setText(text);
+                } else {
+                    mWorkingTextView.setText(working + text);
+                }
+            }
+        };
+
+//        zeroButton = ((Button) view.findViewById(R.id.fragment_remote_control_zeroButton));
+//        zeroButton.setOnClickListener(numberButtonListener);
+//
+//        oneButton = ((Button) view.findViewById(R.id.fragment_remote_control_oneButton));
+//        oneButton.setOnClickListener(numberButtonListener);
+        TableLayout tableLayout = ((TableLayout) view.findViewById(R.id.fragment_remote_control_tableLayout));
+        int number = 1;
+        for (int i = 2; i < tableLayout.getChildCount() - 1; i++) {
+            TableRow row = ((TableRow) tableLayout.getChildAt(i));
+            for (int j = 0; j < row.getChildCount(); j++) {
+                Button button = ((Button) row.getChildAt(j));
+                button.setText("" + number);
+                button.setOnClickListener(numberButtonListener);
+                number++;
+            }
+        }
+//        enterButton = ((Button) view.findViewById(R.id.fragment_remote_control_enterButton));
+//        enterButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                CharSequence working = mWorkingTextView.getText();
+//                if (working.length() > 0) {
+//                    mSelectedTextView.setText(working);
+//                }
+//                mWorkingTextView.setText("0");
+//            }
+//        });
+        TableRow bottomRow = ((TableRow) tableLayout.getChildAt(tableLayout.getChildCount() - 1));
+        Button deleteButton = ((Button) bottomRow.getChildAt(0));
+        deleteButton.setText("Delete");
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWorkingTextView.setText("0");
+            }
+        });
+
+        Button zeroButton = ((Button) bottomRow.getChildAt(1));
+        zeroButton.setText("0");
+        zeroButton.setOnClickListener(numberButtonListener);
+
+        Button enterButton = ((Button) bottomRow.getChildAt(2));
+        enterButton.setText("Enter");
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence working = mWorkingTextView.getText();
+                if (working.length() > 0) {
+                    mSelectedTextView.setText(working);
+                }
+                mWorkingTextView.setText("0");
+            }
+        });
+        return view;
+    }
 }
